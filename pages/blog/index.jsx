0c5352d24c677830b0035bd490536f57f37afbde
krisograbek <krzysztof.ograbek@gmail.com>
2022-08-05 07:59:49 +0200

    show first article, add functions to get articles and convert MD to HTML

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,12 +1,44 @@
+import Link from 'next/link';
 import React from 'react'
 import { Blog } from '../../components';
+import { getAllArticles } from '../api/api';
 
-const BlogHome = () => {
+const BlogHome = ({ allArticles }) => {
+  console.log(allArticles)
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col min-h-screen items-center py-40">
       <Blog />
+      {allArticles.map((article) => (
+        <div key={article.slug}>
+          <p>
+            <Link as={`/blog/${article.slug}`} href="/blog/[slug]">
+              <a className="hover:underline">{article.title}</a>
+            </Link>
+          </p>
+          <p>{article.date}</p>
+          <p>{article.author.name}</p>
+          <div>---</div>
+        </div>
+      ))}
+
     </div>
   )
 }
 
 export default BlogHome;
+
+export const getStaticProps = async () => {
+  const allArticles = getAllArticles([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allArticles },
+  }
+}
+
