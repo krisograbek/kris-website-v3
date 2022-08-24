718eafdb32ef7025c9d68bc95f9e8ac703b4f256
krisograbek <krzysztof.ograbek@gmail.com>
2022-08-24 14:20:30 +0200

    add styling to summary cards

--- components/Summaries/Summaries.jsx
+++ components/Summaries/Summaries.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import AboutAuthor from '../AboutAuthor';
 import SummaryCard from './SummaryCard';
 
 const Summaries = ({ summaries }) => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+    <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 pt-20">
       <div className='lg:col-span-8 col-span-1'>
-        {summaries.map((summary) => (
-          <div key={summary.slug}>
-            <SummaryCard summary={summary} />
+        <div className='bg-white p-4 shadow-lg rounded-lg'>
+          <div>
+            Intro
           </div>
-        ))}
+          {summaries.map((summary) => (
+            <div key={summary.slug} className='border-b last:border-b-0'>
+              <SummaryCard summary={summary} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='lg:col-span-4 col-span-1'>
+        <AboutAuthor />
       </div>
     </div>
   )
--- components/Summaries/SummaryCard.jsx
+++ components/Summaries/SummaryCard.jsx
@@ -1,14 +1,33 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
 const SummaryCard = ({ summary }) => {
   return (
-    <div className='text-white'>
-      <Link as={`/summaries/${summary.slug}`} href="/summaries/[slug]">
-        {summary.slug}
-      </Link>
-      <div>
-        Hi {summary.excerpt}
+    <div className='flex flex-col pt-8 pb-12'>
+      <div className='flex gap-6'>
+        <div className='flex-auto relative h-60'>
+          <Image src={summary.coverImage} alt="cover Image" layout='fill' objectFit='contain' />
+        </div>
+        <div className='w-2/3 flex flex-col justify-center'>
+          <div className='text-2xl pb-2'>
+            {summary.title}
+          </div>
+          <div className='text-xl pb-4'>
+            {summary.author}
+          </div>
+          <div className='text-lg pb-4'>
+            Year: {summary.year}
+          </div>
+          <Link as={`/summaries/${summary.slug}`} href="/summaries/[slug]">
+            <div className='text-xl pb-2 cursor-pointer underline'>
+              Read Summary
+            </div>
+          </Link>
+        </div>
+      </div>
+      <div className='pt-8'>
+        {summary.excerpt}
       </div>
     </div>
   )
