cb82798fb2e293fae745f36cba8d268bbf77d732
krisograbek <krzysztof.ograbek@gmail.com>
2022-08-25 12:02:46 +0200

    adjust styles to the article

--- components/Blog/ArticleDetails.jsx
+++ components/Blog/ArticleDetails.jsx
@@ -5,6 +5,9 @@ import MarkdownComponent from '../MarkdownComponent';
 const ArticleDetails = ({ article }) => {
   return (
     <div className='text-white '>
+      <div className='text-3xl mb-8'>
+        {article.title}
+      </div>
       <div className='mb-8 md:flex md:justify-center'>
         <img src={article.coverImage} alt="cover Image" className='rounded-lg w-full' />
       </div>
